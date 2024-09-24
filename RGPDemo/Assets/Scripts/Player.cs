e7e48b4d823129545b8989383a65b2e0debbda36
Chama992 <135295169+Chama992@users.noreply.github.com>
2024-09-24 20:06:23 +0800

    basic
    dash attack and move

--- RGPDemo/Assets/Scripts/Player.cs
+++ RGPDemo/Assets/Scripts/Player.cs
@@ -11,15 +11,26 @@ public class Player : MonoBehaviour
     private float xInput;
     private int facingDir = 1;
     private bool facingRight = true;
-    [Header("Moving")]
+    [Header("MovingInfo")]
     [SerializeField]private float jumpForce;
     [SerializeField]private float moveSpeed;
     private bool isGround;
     [Header("CollisionInfo")]
     [SerializeField]private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGrount;
+    [Header("DashInfo")]
+    [SerializeField] private float dashDuration;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCoolDown;
+    [SerializeField] private float dashCoolDownTimer;
+    [Header("Attack Info")]
+    [SerializeField] private float comboTime = .3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int attackCombo;
     void Start()
-    {
+    { 
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
     }
@@ -30,10 +41,28 @@ public class Player : MonoBehaviour
         CheckInput();
         MoveMent();
         ColissionChecks();
+        dashTime -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+        dashCoolDownTimer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (dashCoolDownTimer < 0)
+                DashAbility();
+        }
         FlipController();
         AnimatorController();
 
     }
+
+    private void DashAbility()
+    {
+        if (!isAttacking)
+        {
+            dashCoolDownTimer = dashCoolDown;
+            dashTime = dashDuration;
+        }
+    }
+
     /// <summary>
     /// check isground
     /// </summary>
@@ -48,15 +77,35 @@ public class Player : MonoBehaviour
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetMouseButtonDown(0))
+        {
+            AttackEvent();
+        }
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
     }
+
+    private void AttackEvent()
+    {
+        if (!isGround)
+            return;
+        if (comboTimeWindow < 0)
+            attackCombo = 0;
+        comboTimeWindow = comboTime;
+        isAttacking = true;
+    }
+
     /// <summary>
     /// change x volocity
     /// </summary>
     private void MoveMent()
     {
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        if (isAttacking)
+            rb.velocity = Vector2.zero;
+        else if (dashTime > 0)
+            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
+        else
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
     /// <summary>
     /// jump,make y a force 
@@ -72,14 +121,20 @@ public class Player : MonoBehaviour
     private void AnimatorController()
     {
         bool isMoving = xInput != 0;
+        animator.SetFloat("yVelocity", rb.velocity.y);
+        //Debug.Log(rb.velocity.y);
         animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isGround", isGround);
+        animator.SetBool("isDashing", dashTime > 0);
+        animator.SetBool("isAttacking", isAttacking);
+        animator.SetInteger("attackCombo", attackCombo);
     }
     /// <summary>
     /// flip the sprite
     /// </summary>
     private void Flip()
     {
-        facingDir = facingDir = -1;
+        facingDir = facingDir * -1;
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
     }
@@ -99,5 +154,12 @@ public class Player : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+    public void QuitAttack()
+    {
+        attackCombo++;
+        if (attackCombo > 2)
+            attackCombo = 0;
+        isAttacking = false;
     }
 }
